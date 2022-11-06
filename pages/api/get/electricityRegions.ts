39f7f60b5ec0e2d3b8086474e7e7701cfef5223a
DonkeyBlaster <57310593+DonkeyBlaster@users.noreply.github.com>
2022-11-06 14:51:11 -0800

    feat: electricityRegions route

---
+++ pages/api/get/electricityRegions.ts
@@ -0,0 +1,19 @@
+import type {NextApiRequest, NextApiResponse} from "next";
+
+type Data = {
+    status: string
+    validRegions: Partial<{[key: string]: string}>[]
+}
+
+export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+    if (!req.complete) return;
+
+    fetch(`https://beta3.api.climatiq.io/emission-factors/regions`, {
+        headers: new Headers({'Authorization': 'Bearer ' + process.env.CLIMATIQ_API_KEY})
+    })
+        .then(response => response.json())
+        .then(data => {
+            res.status(200).json({"status": "Success", "validRegions": data})
+        }
+    );
+}
