60aee5c92782cef9e90e426b9297caa499896bb6
I-Am-Ok-Am-Coding <123.roychen@gmail.com>
2022-11-06 14:14:46 -0800

    Added Region and Electricity Rate output

--- pages/api/get/electricity.ts
+++ pages/api/get/electricity.ts
@@ -1,19 +1,23 @@
 import type {NextApiRequest, NextApiResponse} from "next";
+
 type Data = {
     status: string
+    kgCO2PerKWh: number | null
 }
 
+const Region = "CA";
+const Energy = 1;
+
 export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
-    var APIKEY = "X0EZY0F728MDH5N29TTTD02P6SGC";
-    var url = "https://beta3.api.climatiq.io/estimate"
+    const url = "https://beta3.api.climatiq.io/estimate"
     
     let data = {
         emission_factor: {
             "id": "electricity-energy_source_grid_mix",
-            "region": "CA"
+            "region": Region
           },
         parameters: {
-            "energy": 1000,
+            "energy": Energy,
             "energy_unit": "kWh" }
           }
     
@@ -21,13 +25,15 @@ export default function handler(req: NextApiRequest, res: NextApiResponse<Data>)
       method: 'POST',
       body: JSON.stringify(data),
       headers: new Headers({
-        'Authorization': 'Bearer ' + APIKEY
+        'Authorization': 'Bearer ' + process.env.CLIMATIQ_API_KEY
       })
     }
     
     fetch(url, fetchData)
     .then(response=>response.json())
+    .then(data1 => {
+      console.log("Region: "+data1.emission_factor.region); 
+      console.log("CO2 kg/kWh "+ data1.co2e);
+      res.status(200).json({"status": "Success", "kgCO2PerKWh": data1.co2ea})})
     
-    .then(data=>{ console.log(data); })
-     res.status(200).json({"status": "Success"})
 }
