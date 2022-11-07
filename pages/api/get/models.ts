9e0088c273982240fadd01bbc59f2f261efb17c4
Michael Wang <41971562+devmwang@users.noreply.github.com>
2022-11-06 18:42:00 -0800

    Updated api to use experimental edge

--- pages/api/get/models.ts
+++ pages/api/get/models.ts
@@ -1,40 +1,64 @@
-import type {NextApiRequest, NextApiResponse} from "next";
+import type { NextApiRequest, NextApiResponse } from "next";
 
 type Data = {
-    status: string
-    models: string[] | null
-}
+    status: string;
+    models: string[] | null;
+};
 
-export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+export default function handler(
+    req: NextApiRequest,
+    res: NextApiResponse<Data>
+) {
     if (!req.complete) return;
     // expecting req.query to contain key-pair {"year": "number", "make": "string"}
     if (!req.query["year"]) {
-        res.status(400).json({"status": "Missing year query parameter", "models": null});
+        res.status(400).json({
+            status: "Missing year query parameter",
+            models: null,
+        });
         return;
     }
     if (!req.query["make"]) {
-        res.status(400).json({"status": "Missing make query parameter", "models": null});
+        res.status(400).json({
+            status: "Missing make query parameter",
+            models: null,
+        });
         return;
     }
 
     // check if the provided make is valid for the provided year using our own api
-    fetch(`http://${process.env.NEXT_PUBLIC_BASE_URL}/api/get/makes?year=${req.query["year"]}`)
-        .then(response => response.json())
-        .then(data => {
+    fetch(
+        `http://${process.env.NEXT_PUBLIC_BASE_URL}/api/get/makes?year=${req.query["year"]}`
+    )
+        .then((response) => response.json())
+        .then((data) => {
             if (!data.makes.includes(req.query["make"])) {
-                res.status(400).json({"status": "Invalid make for provided year", "models": null});
+                res.status(400).json({
+                    status: "Invalid make for provided year",
+                    models: null,
+                });
                 return;
             }
-        }
-    );
+        });
 
     // fetch models from us government emissions json database api
-    fetch(`https://fueleconomy.gov/ws/rest/vehicle/menu/model?year=${req.query["year"]}&make=${req.query["make"]}`, {
-        headers: new Headers({'Accept': 'application/json'})
-    })
-        .then(response => response.json())
-        .then(data => {
-            res.status(200).json({"status": "Success", "models": data.menuItem.map((item: {text: string}) => item.text)})
+    fetch(
+        `https://fueleconomy.gov/ws/rest/vehicle/menu/model?year=${req.query["year"]}&make=${req.query["make"]}`,
+        {
+            headers: new Headers({ Accept: "application/json" }),
         }
-    );
+    )
+        .then((response) => response.json())
+        .then((data) => {
+            res.status(200).json({
+                status: "Success",
+                models: data.menuItem.map(
+                    (item: { text: string }) => item.text
+                ),
+            });
+        });
 }
+
+export const config = {
+    runtime: "experimental-edge",
+};
--- pages/api/get/vehicles.ts
+++ pages/api/get/vehicles.ts
@@ -1,62 +1,87 @@
-import type {NextApiRequest, NextApiResponse} from "next";
+import type { NextApiRequest, NextApiResponse } from "next";
 
 type Data = {
-    status: string
-    vehicles: Partial<{[key: string]: number}>[] | null
-}
+    status: string;
+    vehicles: Partial<{ [key: string]: number }>[] | null;
+};
 
-export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+export default function handler(
+    req: NextApiRequest,
+    res: NextApiResponse<Data>
+) {
     if (!req.complete) return;
     // expecting req.query to contain key-pair {"year": "number", "make": "string"}
     if (!req.query["year"]) {
-        res.status(400).json({"status": "Missing year query parameter", "vehicles": null});
+        res.status(400).json({
+            status: "Missing year query parameter",
+            vehicles: null,
+        });
         return;
     }
     if (!req.query["make"]) {
-        res.status(400).json({"status": "Missing make query parameter", "vehicles": null});
+        res.status(400).json({
+            status: "Missing make query parameter",
+            vehicles: null,
+        });
         return;
     }
     if (!req.query["model"]) {
-        res.status(400).json({"status": "Missing model query parameter", "vehicles": null});
+        res.status(400).json({
+            status: "Missing model query parameter",
+            vehicles: null,
+        });
         return;
     }
 
     // check if the provided make is valid for the provided year using our own api
-    fetch(`http://${process.env.NEXT_PUBLIC_BASE_URL}/api/get/makes?year=${req.query["year"]}`)
-        .then(response => response.json())
-        .then(data => {
-                if (!data.makes.includes(req.query["make"])) {
-                    res.status(400).json({"status": "Invalid make for provided year", "vehicles": null});
-                    return;
-                }
+    fetch(
+        `http://${process.env.NEXT_PUBLIC_BASE_URL}/api/get/makes?year=${req.query["year"]}`
+    )
+        .then((response) => response.json())
+        .then((data) => {
+            if (!data.makes.includes(req.query["make"])) {
+                res.status(400).json({
+                    status: "Invalid make for provided year",
+                    vehicles: null,
+                });
+                return;
             }
-        );
+        });
 
     // check if the provided model is valid for the provided year and make using our own api
-    fetch(`http://${process.env.NEXT_PUBLIC_BASE_URL}/api/get/models?year=${req.query["year"]}&make=${req.query["make"]}`)
-        .then(response => response.json())
-        .then(data => {
-                if (!data.models.includes(req.query["model"])) {
-                    res.status(400).json({"status": "Invalid model for provided year and make", "vehicles": null});
-                    return;
-                }
+    fetch(
+        `http://${process.env.NEXT_PUBLIC_BASE_URL}/api/get/models?year=${req.query["year"]}&make=${req.query["make"]}`
+    )
+        .then((response) => response.json())
+        .then((data) => {
+            if (!data.models.includes(req.query["model"])) {
+                res.status(400).json({
+                    status: "Invalid model for provided year and make",
+                    vehicles: null,
+                });
+                return;
             }
-        );
-
+        });
 
     // fetch models from us government emissions json database api
-    fetch(`https://fueleconomy.gov/ws/rest/vehicle/menu/options?year=${req.query["year"]}&make=${req.query["make"]}&model=${req.query["model"]}`, {
-        headers: new Headers({'Accept': 'application/json'})
-    })
-        .then(response => response.json())
-        .then(data => {
+    fetch(
+        `https://fueleconomy.gov/ws/rest/vehicle/menu/options?year=${req.query["year"]}&make=${req.query["make"]}&model=${req.query["model"]}`,
+        {
+            headers: new Headers({ Accept: "application/json" }),
+        }
+    )
+        .then((response) => response.json())
+        .then((data) => {
             let returnable = [data.menuItem];
             try {
-                let x = data.menuItem[1]
+                let x = data.menuItem[1];
             } catch (e) {
                 returnable = data.menuItem;
             }
-            res.status(200).json({"status": "Success", "vehicles": returnable});
-        }
-    );
+            res.status(200).json({ status: "Success", vehicles: returnable });
+        });
 }
+
+export const config = {
+    runtime: "experimental-edge",
+};
