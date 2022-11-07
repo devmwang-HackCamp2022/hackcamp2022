04990c7e65fec23a3a848dae7484f5724212f392
Michael Wang <41971562+devmwang@users.noreply.github.com>
2022-11-06 16:30:41 -0800

    Updated makes api

--- pages/api/get/makes.ts
+++ pages/api/get/makes.ts
@@ -1,25 +1,41 @@
-import type {NextApiRequest, NextApiResponse} from "next";
+import type { NextApiRequest, NextApiResponse } from "next";
 
 type Data = {
-    status: string
-    makes: string[] | null
-}
+    status: string;
+    makes: string[] | null;
+};
 
-export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse<Data>
+) {
     if (!req.complete) return;
     // Expecting req.query to contain key-pair {"year": "number"}
     if (!req.query["year"]) {
-        res.status(400).json({"status": "Missing year query parameter", "makes": null});
+        res.status(400).json({
+            status: "Missing year query parameter",
+            makes: null,
+        });
         return;
     }
 
-    // fetch makes from us government emissions json database api
-    fetch(`https://fueleconomy.gov/ws/rest/vehicle/menu/make?year=${req.query["year"]}`, {
-        headers: new Headers({'Accept': 'application/json'})
-    })
-        .then(response => response.json())
-        .then(data => {
-            res.status(200).json({"status": "Success", "makes": data.menuItem.map((item: {text: string}) => item.text)})
-        }
-    );
+    return new Promise<void>((resolve, reject) => {
+        // fetch makes from us government emissions json database api
+        fetch(
+            `https://fueleconomy.gov/ws/rest/vehicle/menu/make?year=${req.query["year"]}`,
+            {
+                headers: new Headers({ Accept: "application/json" }),
+            }
+        )
+            .then((response) => response.json())
+            .then((data) => {
+                res.status(200).json({
+                    status: "Success",
+                    makes: data.menuItem.map(
+                        (item: { text: string }) => item.text
+                    ),
+                });
+                resolve();
+            });
+    });
 }
